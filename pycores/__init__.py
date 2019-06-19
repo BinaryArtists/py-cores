d8dc5b5963ee7badf8011d975326a4b59787cff2
fallen.ink <fengzilijie@qq.com>
2019-06-19 22:43:28 +0800

    +

--- pycores/__init__.py
+++ pycores/__init__.py
@@ -1,1 +1,3 @@
 from .core import *
+from .notification import *
+from .baserepo import *
--- pycores/__version__.py
+++ pycores/__version__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-VERSION = (0, 1, 4)
+VERSION = (0, 1, 8)
 
 __version__ = '.'.join(map(str, VERSION))
